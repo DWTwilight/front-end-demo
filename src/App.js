9f5d3a6338d3dadf54b7195e7afbcc01e7eb6cec
Tianci Zhao <tianci.zhao@thoughtworks.com>
2023-07-02 20:09:19 +0800

    feat: add register form

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Title from "./components/Title";
+import RegisterForm from "./components/RegisterForm";
 import "./styles/app.scss";
 
 function App() {
   return (
     <div className="App">
       <Title />
+      <RegisterForm />
     </div>
   );
 }
